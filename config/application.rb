38447b8a28b3aece35c439c66d2e46ec0fd33fc4
mac-adam-chaieb <moe.chaieb@shopify.com>
2018-11-22 18:36:14 -0500

    Remove rails/all

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'rake'
+require "action_controller/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
